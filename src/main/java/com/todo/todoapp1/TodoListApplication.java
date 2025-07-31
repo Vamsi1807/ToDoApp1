7160d0599d4c86dca165c828dace625955256b2d
Vamsi1807 <vamsigopavarapu07@gmail.com>
2025-07-31 23:33:36 +0530

    changes done

--- src/main/java/com/todo/todoapp1/TodoListApplication.java
+++ src/main/java/com/todo/todoapp1/TodoListApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TodoListApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TodoListApplication.class, args);
     }
 
--- src/main/java/com/todo/todoapp1/config/WebConfig.java
+++ src/main/java/com/todo/todoapp1/config/WebConfig.java
@@ -1,23 +1,8 @@
 package com.todo.todoapp1.config;
 
-//import org.springframework.boot.web.servlet.*;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.support.ErrorPageFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig {
-
-    @Bean
-    public ErrorPageFilter errorPageFilter() {
-        return new ErrorPageFilter();
-    }
-
-    @Bean
-    public FilterRegistrationBean<ErrorPageFilter> disableErrorPageFilter(ErrorPageFilter filter) {
-        FilterRegistrationBean<ErrorPageFilter> registration = new FilterRegistrationBean<>(filter);
-        registration.setEnabled(false);
-        return registration;
-    }
+    // No ErrorPageFilter beans needed for WAR deployments
 }
